e24b6e5146a929f72f2410d327c05a35bedb1db7
Elisabet Ramirez <elisaram@gmail.com>
2023-10-19 12:40:54 +0200

    Delete crops

--- app/models/crop.rb
+++
@@ -1,7 +0,0 @@
-class Crop < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 20 }
-  validates :month, presence: false
-  validates :moon, presence: true
-
-  belongs_to :plant, class_name: 'Plant'
-end
