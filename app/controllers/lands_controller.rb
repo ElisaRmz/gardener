0ef307c451d5bf13e9543775feb4ccb70ce62f8b
Elisabet Ramirez <elisaram@gmail.com>
2023-10-24 13:37:55 +0200

    update User and Lands

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -1,4 +1,22 @@
 class LandsController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+    @land = Land.new
+  end
+
+  def create
+    @land = Land.new(land_params)
+    if @land.save
+      redirect_to @land, notice: 'La tierra ha sido creada exitosamente.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def land_params
+    params.require(:land).permit(:nombre)
+  end
 end
